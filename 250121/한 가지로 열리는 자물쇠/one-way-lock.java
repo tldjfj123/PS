f86a192629a5880bd4b331b9546f16f9ac925ee4
Ahn <dksckd0804@naver.com>
2025-01-21 22:02:17 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 84ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250121/한 가지로 열리는 자물쇠/one-way-lock.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        int res = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    if (Math.abs(i-a) <= 2 || Math.abs(j - b) <= 2 || Math.abs(k - c) <= 2) {
+                        res++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(res);
+
+
+    }
+}
