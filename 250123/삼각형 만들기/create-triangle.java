895952ba8e2bc365ba20aa569353680485eea298
Ahn <dksckd0804@naver.com>
2025-01-23 16:00:29 +0900

    Title: 삼각형 만들기, Time: 111ms, Memory: 9MB, Status: Passed - CodeTree

--- 250123/삼각형 만들기/create-triangle.java
+++ 250123/삼각형 만들기/create-triangle.java
@@ -51,6 +51,6 @@ public class Main {
     }
 
     static int getWidth(int[] x, int[] y, int[] z) {
-        return Math.abs((x[0]*y[1] + y[0]*z[1] + z[0]*x[1]) - (y[0]*x[0] + z[0]*y[1] + x[0]*z[1]));
+        return Math.abs((x[0]*y[1] + y[0]*z[1] + z[0]*x[1]) - (y[0]*x[1] + z[0]*y[1] + x[0]*z[1]));
     }
 }
