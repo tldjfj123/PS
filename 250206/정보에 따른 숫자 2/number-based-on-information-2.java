6897fe75b85334d40253abd59d2789de2aedc4a4
Ahn <dksckd0804@naver.com>
2025-02-06 16:23:11 +0900

    Title: 정보에 따른 숫자 2, Time: 102ms, Memory: 9MB, Status: Passed - CodeTree

--- 250206/정보에 따른 숫자 2/number-based-on-information-2.java
+++ 250206/정보에 따른 숫자 2/number-based-on-information-2.java
@@ -13,7 +13,7 @@ public class Main {
         a = Integer.parseInt(st.nextToken());
         b = Integer.parseInt(st.nextToken());
 
-        arr = new String[b+1];
+        arr = new String[1001];
 
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
@@ -21,9 +21,7 @@ public class Main {
             String v = st.nextToken();
             int idx = Integer.parseInt(st.nextToken());
 
-            if (idx <= b) {
-                arr[idx] = v;
-            }
+            arr[idx] = v;
             
         }
 
@@ -31,11 +29,11 @@ public class Main {
 
         // 기준이 되는 index를 정해주는 for 문
         for (int i = a; i <= b; i++) {
-            // a, b까지 모든 범위를 탐색하여 S, N의 최기 idx 값을 갱신함
-            int d1 = b;
-            int d2 = b;
+            // 1, 1000 까지 모든 범위를 탐색하여 S, N의 최기 idx 값을 갱신함
+            int d1 = 1001;
+            int d2 = 1001;
 
-            for (int j = a; j <= b; j++) {
+            for (int j = 1; j <= 1000; j++) {
                 if (arr[j] != null) {
                     if (arr[j].equals("S")) {
                         d1 = Math.min(d1, Math.abs(i - j));
@@ -45,6 +43,8 @@ public class Main {
                 }
                 
             }
+
+        
 
             if (d1 <= d2) {
                 cnt++;
