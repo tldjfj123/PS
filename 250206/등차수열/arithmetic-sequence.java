f1f1dcff6a076ee152857ce2bd00a12d669b6cd7
Ahn <dksckd0804@naver.com>
2025-02-06 22:38:53 +0900

    Title: 등차수열, Time: 114ms, Memory: 9MB, Status: Passed - CodeTree

--- 250206/등차수열/arithmetic-sequence.java
+++ 250206/등차수열/arithmetic-sequence.java
@@ -21,26 +21,24 @@ public class Main {
         if (arr.length == 1) {
             System.out.println(0);
         } else { // 아닌경우 : 숫자 2개 정해서 차이 / 2 == 0 인지 확인
-            int cnt = 0;
+            int res = 0;
 
-            for (int i = 0; i < n; i++) {
-                for (int j = i+1; j < n; j++) {
-                    if (Math.abs(arr[i] - arr[j]) % 2 == 0) {
-                        cnt++;
+            for (int k = 1; k <= 100; k++) { // 기준이 되는 k 
+                int cnt = 0;
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = i+1; j < n; j++) {
+                        if (Math.abs(arr[i] - k) == Math.abs(arr[j] - k)) {
+                            cnt++;
+                        }
                     }
                 }
+
+                res = Math.max(cnt, res);
             }
 
-            System.out.println(cnt);
+            System.out.println(res);
         }
-
-
-
-
-
-
-
-        
 
     }
 }
