cbff36461f6aae7f3a8446d52e03f658f658a876
Ahn <dksckd0804@naver.com>
2025-02-26 20:36:36 +0900

    Title: 최대로 겹치는 구간, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

--- 250226/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 250226/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -10,13 +10,13 @@ public class Main {
         n = Integer.parseInt(br.readLine());
 
 
-        arr = new int[100];
+        arr = new int[201];
 
         for (int i = 0; i < n; i++) {       
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken()) + 100;
+            int b = Integer.parseInt(st.nextToken()) + 100;
 
             for (int j = a; j < b; j++) {
                 arr[j]++;
@@ -25,7 +25,7 @@ public class Main {
 
         int res = 0;
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 200; i++) {
             res = Math.max(res, arr[i]);
         }
 
