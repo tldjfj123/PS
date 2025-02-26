f240091d5c3f7ed8543464b4603577f23c048aab
Ahn <dksckd0804@naver.com>
2025-02-26 19:57:23 +0900

    Title: Date to Date, Time: 78ms, Memory: 9MB, Status: Passed - CodeTree

--- 250226/Date to Date/date-to-date.java
+++ 250226/Date to Date/date-to-date.java
@@ -14,30 +14,17 @@ public class Main {
         int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         int total1 = 0;
-
-        for (int i = 0; i < m1; i++) {
-            if (i == 0) {
-                continue;
-            }
-
+        for (int i = 0; i < m1 - 1; i++) {  
             total1 += days[i];
         }
-
-        total1 += d1;        
+        total1 += d1; 
 
         int total2 = 0;
-
-        for (int i = 0; i < m2; i++) {
-            if (i == 0) {
-                continue;
-            }
-
+        for (int i = 0; i < m2 - 1; i++) {  
             total2 += days[i];
         }
+        total2 += d2; 
 
-        total2 += d2;
-
-
-        System.out.println(total2 - total1);
+        System.out.println(total2 - total1 + 1);
     }
 }
