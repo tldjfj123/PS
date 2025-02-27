1ad117006ecf3f10acff65aa5265a7a1de5846ba
Ahn <dksckd0804@naver.com>
2025-02-27 21:18:21 +0900

    Title: 왔다 갔던 구역 2, Time: 78ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250227/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int[] arr;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        
+        arr = new int[2001];
+        int idx = 1000;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int v = Integer.parseInt(st.nextToken());
+            String o = st.nextToken();
+
+            if (o.equals("L")) {
+                for (int j = 0; j < v; j++) {
+                    arr[idx]++;
+                    idx--;
+                }
+            } else {
+                for (int j = 0; j < v; j++) {
+                    arr[idx]++;
+                    idx++;
+                }
+            }
+        }       
+
+        int res = 0;
+        for (int i = 0; i < 2001; i++) {
+            if (arr[i] >= 2) {
+                res++;
+            }
+        }
+
+        System.out.println(res);
+    }
+}
