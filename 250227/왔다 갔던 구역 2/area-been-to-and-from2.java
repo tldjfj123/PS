a76153e63d55dae3b8fabc106d42c1d9a6dd7a8f
Ahn <dksckd0804@naver.com>
2025-02-27 22:29:39 +0900

    Title: 왔다 갔던 구역 2, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

--- 250227/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250227/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -4,38 +4,34 @@ import java.io.*;
 public class Main {
     static int n;
     static int[] arr;
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
         
-        arr = new int[2001];
-        int idx = 1000;
+        arr = new int[2001];  // 인덱스: 0~2000 사용
+        int idx = 1000;  // 시작 위치 (0을 1000으로 변환)
+
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());  // 이동 거리
+            String o = st.nextToken();  // 방향 (L or R)
 
-            int v = Integer.parseInt(st.nextToken());
-            String o = st.nextToken();
-
-            // arr[idx]++; // 출발점 +1 
-            
-            if (o.equals("L")) {
-                for (int j = 0; j < v; j++) {
+            for (int j = 0; j < v; j++) {
+                if (o.equals("L")) {
+                    arr[idx - 1]++;  // 왼쪽으로 이동하는 경우, 이전 구간을 증가
                     idx--;
-                    arr[idx]++;
-                }
-            } else {
-                for (int j = 0; j < v; j++) {
+                } else {
+                    arr[idx]++;  // 오른쪽으로 이동하는 경우, 현재 구간을 증가
                     idx++;
-                    arr[idx]++;
                 }
             }
-        }       
+        }
 
         int res = 0;
         for (int i = 0; i < 2001; i++) {
-            if (arr[i] >= 2) {
-                // System.out.println(i);
+            if (arr[i] >= 2) {  // 구간이 2번 이상 지나갔는지 확인
                 res++;
             }
         }
