72ebec91ed23d228a15737960da5830918eed3d3
Ahn <dksckd0804@naver.com>
2025-06-09 23:03:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.63 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,33 +2,32 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // HashMap에 참석자들 정보 담기
         Map<String, Integer> map = new HashMap<String, Integer>();
-        String res = "";
         
-        // 동명이인 처리
+        // map에 사람 추가하기
         for (String p : participant) {
-            if (map.containsKey(p)) {
-                map.put(p, map.get(p) + 1);
-            } else {
+            if (!map.containsKey(p)) {
                 map.put(p, 1);
+            } else {
+                int tmp = map.get(p);
+                map.put(p, tmp+1);
             }
         }
         
-        // 완주자들 확인 처리
         for (String c : completion) {
-            if (map.containsKey(c)) { 
-                map.put(c, map.get(c) - 1);
-            }
+            map.put(c, map.get(c) - 1);
         }
         
-        // map의 키값들로 값을 조회하면서 value가 0이 아닌 것 return (완주 못한 놈)
-        for (String key : map.keySet()) {
-            if (map.get(key) != 0) {
-                res = key;
+        // System.out.println(map);
+        
+        String res = "";
+        for (String p : participant) {
+            if (map.get(p) != 0) {
+                res = p;
                 break;
             }
         }
+        
         return res;
     }
 }
